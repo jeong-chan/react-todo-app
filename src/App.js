de9c3f26b51c331d732dcc2805c64b19a19c7bca
정찬우 <jeong_2240@naver.com>
2022-10-17 04:00:37 +0900

    따라하며 배우는 리액트 A-Z 3강

--- src/App.js
+++ src/App.js
@@ -1,102 +1,57 @@
-import React, {Component} from "react";
+import React, {useState, useCallback} from "react";
+import Lists from "./components/Lists";
+import Form from "./components/Form";
 import "./App.css";
 
-export default class App extends Component {
+const initialTodoData = localStorage.getItem('todoData') ? JSON.parse(localStorage.getItem("todoData")):[];
 
-  state = {
-    todoData : [],
-    value : ""
-  }
+export default function App() {
 
-  btnStyle ={
-    color : "#fff",
-    border : "none",
-    padding : "5px 9px",
-    borderRadius : "50%",
-    cursor : "pointer",
-    float : "right"
-  }
+  const [todoData, setTodoData] = useState(initialTodoData);
+  const [value, setValue] = useState("");
 
-  getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none"
-    }
-  }
-
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    this.setState({todoData : newTodoData});
-    console.log('newTodoData', newTodoData);
-  };
-
-  handleChange = (e) => {
-    console.log('e', e.target.value);
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     // form 안에 input을 전송할 때 페이지 리로드 되는 것을 막아줌
     e.preventDefault();
 
     //새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       completed: false,
     };
 
     //원래 있던 할 일에 새로운 할 일 더해주기 && 입력칸 비우기
-    this.setState({todoData: [...this.state.todoData, newTodo], value:""});
-
+    setTodoData(prev => [...prev,newTodo]);
+    localStorage.setItem('todoData', JSON.stringify([...todoData,newTodo]));
+    setValue("");
   }
 
-  handleCompleChange = (id) => {
-    let newTodoData = this.state.todoData.map((data) => {
-      if(data.id === id){
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    this.setState({ todoData: newTodoData });
+  const handleClick = useCallback((id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(newTodoData);
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
+    console.log('newTodoData', newTodoData);
+  },[todoData]);
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
   }
 
-  render() {
     return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+          <div className="flex justify-between mb-3">
             <h1>할 일 목록</h1>
+            <button onClick={handleRemoveClick}>Delete All</button>
           </div>
 
-        {this.state.todoData.map((data) => (
-          <div style={this.getStyle(data.completed)} key={data.id}>
-            <input type="checkbox" defaultChecked={false} onChange={()=> this.handleCompleChange(data.id)}/>
-              {data.title}
-            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
-          </div>
-        ))}
-
-          <form style={{ display: 'flex'}} onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="value"
-              style={{flex: "10", padding: "5px"}}
-              placeholder="해야 할 일을 입력하세요."
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{flex: "1"}}
-            /> 
-          </form>
-
+          <Lists todoData={todoData} setTodoData={setTodoData} handleClick={handleClick}/>
+          <Form value={value} setValue={setValue} handleSubmit={handleSubmit}/>
+          
         </div>
       </div>
-    )
-  }
+    );
+
 }
